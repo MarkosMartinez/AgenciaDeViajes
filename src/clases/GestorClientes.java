5f27f808e4b63aab20cf7c8c3e69545549c0606f
nagore <znagoresanchez@gmail.com>
2023-02-07 09:29:30 +0100

    menu clientes

--- src/clases/GestorClientes.java
+++ src/clases/GestorClientes.java
@@ -1,5 +1,38 @@
 package clases;
 
-public class GestorClientes {
+import java.util.Scanner;
 
-}
+public class GestorClientes {
+	public void run(Scanner scan) {
+		int opcion_menu;
+
+		do {
+			Menu.menuClientes();;
+			opcion_menu = Integer.parseInt(scan.nextLine());
+
+			switch (opcion_menu) {
+			case Menu.REGISTRAR_CLIENTES:
+				
+				break;
+			case Menu.DAR_BAJA:
+				
+				
+				break;
+			case Menu.MODIFICAR_CLIENTE:
+				
+				
+				break;
+			case Menu.SALIR:
+				System.out.println("\nAgur!");
+				break;
+			default:
+				System.out.println(Colores.ROJO + "Opcion no valida!\nIntentalo de nuevo." + Colores.NEGRO);
+			}
+
+		} while (opcion_menu != Menu.SALIR);
+		
+	}
+
+		
+	}
+
