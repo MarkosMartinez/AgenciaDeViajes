41cf543076e598f33aff55f5d16a04e7322842af
nagore <znagoresanchez@gmail.com>
2023-02-07 09:01:29 +0100

    VARIABLES

---
+++ src/clases/Menu.java
@@ -0,0 +1,31 @@
+package clases;
+
+public class Menu {
+	
+	//menu principal
+	static final int GESTIONAR_CLIENTES=1;
+	static final int GESTIONAR_RESERVAS=2;
+	static final int GESTIONAR_HOTELES=3;
+	
+	//SALIR
+	
+	static final int SALIR =0;
+	
+	//MENU CLIENTES		
+	static final int REGISTRAR_CLIENTES=1;
+	static final int DAR_BAJA=2;
+	static final int MODIFICAR_CLIENTE=3;
+	
+	
+	// MENU RESERVA
+	static final int REALIZAR_RESERVA=1;
+	static final int ANULAR_RESERVA=2;
+	
+	//MENU HOTELES
+	static final int REGISTRAR_HOTEL=1;
+	static final int CREAR_HABITACION=2;
+	
+	
+	
+
+}
