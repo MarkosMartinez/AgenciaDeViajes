91757e7be57f4990fe133b909c4e10d8edf4fcc0
nagore <znagoresanchez@gmail.com>
2023-02-07 09:59:29 +0100

    registrar cliente

--- src/clases/GestorClientes.java
+++ src/clases/GestorClientes.java
@@ -1,18 +1,22 @@
 package clases;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class GestorClientes {
-	public void run(Scanner scan) {
+	public void run(Scanner scan) throws SQLException {
 		int opcion_menu;
-
+		GestorBBDD gestor = new GestorBBDD();
+		FormularioDeDatos formulario= new FormularioDeDatos();
 		do {
 			Menu.menuClientes();
 			opcion_menu = Integer.parseInt(scan.nextLine());
 
 			switch (opcion_menu) {
 			case Menu.REGISTRAR_CLIENTES:
-				
+				Clientes cliente=formulario.pedirDatosClientes(scan);
+				gestor.altaCliente(cliente);
+				Visor.mostrarMensajeCorrecto("Usuario Insertado correctamente");
 				break;
 			case Menu.DAR_BAJA:
 				
