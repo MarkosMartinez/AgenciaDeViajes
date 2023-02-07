eda5dd37c0a18e165b535122a87778a4a686f33c
MarkosMartinez <ikbdk@plaiaundi.net>
2023-02-07 09:52:22 +0100

    Alta de cliente creado

--- src/clases/GestorBBDD.java
+++ src/clases/GestorBBDD.java
@@ -1,5 +1,22 @@
 package clases;
 
-public class GestorBBDD {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class GestorBBDD extends Conector{ //TODO Es extends?
+	Conector conector = new Conector();
+	
+	//Clientes ----------------------------------------------------
+	public void altaCliente(Clientes cliente) throws SQLException{
+		conector.conectar();
+		PreparedStatement alta = conector.getCon().prepareStatement("INSERT INTO clientes (dni, nombre, apellidos, direccion, localidad) VALUES (?,?,?,?,?);");
+		alta.setString(1, cliente.getDni());
+		alta.setString(2, cliente.getNombre());
+		alta.setString(3, cliente.getApellidos());
+		alta.setString(4, cliente.getDireccion());
+		alta.setString(4, cliente.getLocalidad());
+		alta.execute();
+		conector.cerrar();	
+	}
 
 }
