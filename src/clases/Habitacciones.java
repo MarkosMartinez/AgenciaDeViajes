63e0f7af75da1e1873aa841254b63aedebeb076c
nagore <znagoresanchez@gmail.com>
2023-02-07 08:47:27 +0100

    getters and settes

--- src/clases/Habitacciones.java
+++ src/clases/Habitacciones.java
@@ -2,6 +2,46 @@ package clases;
 
 public class Habitacciones {
 
+	private int id;
+	private int id_hotel;
+	private int numero;
+	private String descripcion;
+	private double precio;
 	
+	
+	///getter and setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getId_hotel() {
+		return id_hotel;
+	}
+	public void setId_hotel(int id_hotel) {
+		this.id_hotel = id_hotel;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	
+
 	
 }
