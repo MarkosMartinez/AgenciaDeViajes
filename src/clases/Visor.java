16a320ab3bdb741a90df5b5376d1c9f324377fef
MarkosMartinez <ikbdk@plaiaundi.net>
2023-02-14 08:57:27 +0100

    Visores del cliente y reservas creado

--- src/clases/Visor.java
+++ src/clases/Visor.java
@@ -22,6 +22,21 @@ public class Visor {
 			System.out.println("ID de la habitacion: " + habitacion.getId() + "\nID del hotel: " + habitacion.getId_hotel() + "\nNumero de la habitacion: " + habitacion.getNumero() + "\nDescipcion de la habitacion: " + habitacion.getDescripcion() + "\nPrecio de la habitacion: "+habitacion.getPrecio() + "\n");
 		}
 	}
+	
+	public static void mostrarReservas(ArrayList<Reserva> reservas) {
+		System.out.println("Estos son todas las reservas ordenadas: ");
+		for (Reserva reserva : reservas) {
+			System.out.println("ID de la reserva: " + reserva.getId() + "\nID de la habitacion: " + reserva.getId_habitacion() + "\nDNI: " + reserva.getDni() + "\nDesde: " + reserva.getDesde() + "\nHasta: "+reserva.getHasta() + "\n");
+		}
+	}
 
+	public static void mostrarClientes(ArrayList<Cliente> clientes) {
+		System.out.println("Estos son todas los clientes ordenados: ");
+		for (Cliente cliente : clientes) {
+			System.out.println("DNI: " + cliente.getDni() + "\nNombre: " + cliente.getNombre() + "\nApellido: " + cliente.getApellidos() + "\nDireccion: " + cliente.getDireccion() + "\nLocalidad: "+ cliente.getLocalidad() + "\n");
+		}
+	}
+	
+	
 	
 }
